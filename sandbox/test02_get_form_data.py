dcc4228a88979d16a875075ec27e0ddfe50f39c9
Olivier Delrieu (maxwell) <olivier@15kay.fr>
2024-04-05 21:44:18 +0200

    doc

---
+++ sandbox/test02_get_form_data.py
@@ -0,0 +1,77 @@
+# using conda 'syntrillo' environment
+
+# query from https://docs.gethealthie.com/docs/#querying-filled-out-forms
+# use https://docs.gethealthie.com/docs/explorer to get parameters
+
+import requests
+import os
+import json
+from dotenv import load_dotenv
+
+# Set up the GraphQL query
+query = '''
+query formAnswerGroups(
+  $date: String,
+  $custom_module_form_id: ID,
+  $user_id: String,
+  ) {
+  formAnswerGroups(
+    date: $date,
+    custom_module_form_id: $custom_module_form_id,
+    user_id: $user_id,
+    ) {
+    id
+    name
+    created_at
+    user_id
+    finished
+    form_answers {
+      label
+      answer
+      # id
+    }
+  }
+}
+'''
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Get the API key from the environment variables
+api_key = os.getenv('API_KEY')
+
+# Check if the API key is available
+if api_key is None:
+    print("API key not found. Make sure it's defined in the .env file.")
+    exit()
+
+# Set up the request headers with the API key
+headers = {
+    'Authorization': f'Basic {api_key}',
+    'AuthorizationSource': 'API'
+}
+
+# Set up the GraphQL variables
+variables = {
+    # 'date': "2021-10-29",  # Example date value
+    # 'custom_module_form_id': "11",  # Example custom_module_form_id value
+    'user_id' : '1035117'
+}
+
+# Set up the GraphQL endpoint URL
+url = 'https://staging-api.gethealthie.com/graphql'
+
+# Make the HTTP POST request to the Healthie API
+response = requests.post(url, json={'query': query, 'variables': variables}, headers=headers)
+
+# Check if the request was successful
+# Handle the response as needed
+if response.status_code == 200:
+    # print(response.json())
+    # Assuming response is the variable holding the JSON response
+    response_data = response.json()
+
+    # Pretty print the JSON response
+    print(json.dumps(response_data, indent=4))
+else:
+    print("Error:", response.text)
---
+++ sandbox/test03_webhook.py
@@ -0,0 +1,41 @@
+
+# https://docs.gethealthie.com/docs/#examples
+
+# webhook : form_answer_group.created : When a Patient completes a Form (intake, program or charting)
+
+# testing endpoint at PythonAnywhere
+
+# the payload for form_answer_group.created is
+#       {"resource_id": 390125, "resource_id_type": "FormAnswerGroup", "event_type": "form_answer_group.created"}
+# where resource_id  is specific to the form and the patient. Same is as in test02 :
+#        {
+#            "data": {
+#                "formAnswerGroups": [
+#                    {
+#                        "id": "390125",
+
+
+import requests
+import json
+
+# Define the payload to be sent in the POST request
+payload = {
+    "event": "appointment_scheduled",
+    "data": {
+        "client_id": "123456",
+        "appointment_date": "2024-04-05",
+        "appointment_time": "10:00 AM",
+        "provider_id": "789012"
+    }
+}
+
+# Define the URL of your Flask application
+url = 'https://syntrillo.pythonanywhere.com/endpoint'
+
+# Send the POST request with the payload
+response = requests.post(url, json=payload)
+
+# Print the response from the server
+print(response.status_code)
+print(response.json())
+
