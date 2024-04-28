3eafb72aa75da44895078ee06a20ddadfdcdf3ac
Olivier Delrieu (maxwell) <olivier@15kay.fr>
2024-04-28 09:41:20 +0200

    vcn chat poc

--- modules/healthie/virtual_care_navigator.py
+++ modules/healthie/virtual_care_navigator.py
@@ -9,6 +9,9 @@ sys.path.append(os.path.dirname(SCRIPT_DIR))
 
 from healthie.utils import HealthieAPIUtils
 
+# the Virtual Care Navigator Healthie id (ie provider id)
+VCN_ID : str ='1108460'
+
 class HealthieAPIVirtualCareNavigator(HealthieAPIUtils):
     """
     A class extending HealthieAPIUtils to handle virtual care navigator operations.
@@ -21,4 +24,183 @@ class HealthieAPIVirtualCareNavigator(HealthieAPIUtils):
         dotenv_path: str = None,
     ):
         super().__init__(api_key, organization, dotenv_path)
+
+
+    def endpoint(
+        self,
+        data : dict  = None
+    ) :
+        """
+            {"resource_id": 260040, "resource_id_type": "Note", "event_type": "message.created", "changed_fields": []}
+        """
+
+        # get whole conversation object
+        note_id = data['resource_id']
+        conversation = self.get_conversation_from_note_id(note_id=note_id)
+
+        # test if owner if VCN account
+        owner_id = conversation['conversation']['owner']['id']
+        if owner_id == VCN_ID :
+
+            # get all the content of the conversation
+            prompt = self.notes_to_prompt(notes=conversation['conversation']['notes'])
+
+            # send new message
+            response = self.create_note(
+                conversation_id=conversation['conversation']['id'],
+                content='blob',
+                user_id=VCN_ID,
+            )
+
+        else:
+            response = None
+
+
+        return response
+
+
+    def get_conversation_from_note_id(
+        self,
+        note_id : str = None,
+    ):
+        """
+        retreives the whole conversation from a note id:
+
+        returns:
+            dict : https://docs.gethealthie.com/schema/conversation.doc
+        """
+
+        # get conversation id from note id
+        note = self.send_query(
+            query="""
+                query note($id: ID) {
+                    note(id: $id) {
+                        conversation_id
+                    }
+                }
+                """,
+            variables={'id': note_id}
+        )
+        conversation_id = note['note']['conversation_id']
+
+        # get conversation from its id
+        conversation = self.send_query(
+            query="""
+                query getConversation($id: ID) {
+                    conversation(id: $id) {
+                        id
+                        name
+                        owner {
+                            id
+                            name
+                        }
+                        conversation_memberships_count
+                        includes_multiple_clients
+                        invitees {
+                            id
+                        }
+                        patient_id
+                        notes {
+                            content
+                        }
+                    }
+                }
+                """,
+            variables={'id': conversation_id}
+        )
+        conversation_id = note['note']['conversation_id']
+
+        return conversation
+
+    def notes_to_prompt(
+        self,
+        notes : list = None,
+    ):
+        """
+        stacks all contents of notes into a multiline text. Each content is separated from the next with a newline.
+
+        Parameters:
+            notes: list of healthie 'note' objects, with 'content' : https://docs.gethealthie.com/schema/note.doc
+
+        Returns:
+            str: Multiline text containing the contents of all notes separated by newline characters.
+
+        """
+        if notes is None:
+            return ""  # Return an empty string if no notes are provided
+
+        # Iterate over the list of notes and concatenate their contents with newline characters
+        multiline_text = "\n".join(note["content"] for note in notes)
+
+        return multiline_text
+
+    def create_note(
+        self,
+        conversation_id : str = None,
+        content : str = None,
+        user_id : str = None,
+    ):
+        """
+        add a new note in a conversation
+        See : https://docs.gethealthie.com/docs/#createconversation-mutation
+
+        """
+
+        # get conversation id from note id
+        response = self.send_query(
+            query="""
+                    mutation createNote(
+                    $user_id: String
+                    $content: String
+                    $conversation_id: String
+                    $attached_image_string: String
+                    $scheduled_at: String
+                    $org_chat: Boolean
+                    $hide_org_chat_confirmation: Boolean
+                    ) {
+                    createNote(
+                        input: {
+                        user_id: $user_id
+                        content: $content # Content of the note
+                        conversation_id: $conversation_id
+                        attached_image_string: $attached_image_string
+                        scheduled_at: $scheduled_at # for scheduling notes, time note will be sent
+                        org_chat: $org_chat # Pass `true` when creating a note by someone who is not the conversation owner (e.g., by another provider on the client's care team)
+                        hide_org_chat_confirmation: $hide_org_chat_confirmation # When True, will hide org chat confirmation modal
+                        }
+                    ) {
+                        note {
+                            id
+                            content
+                            user_id
+                        }
+                        messages {
+                            field
+                            message
+                        }
+                    }
+                }
+                """,
+            variables={
+                'conversation_id': conversation_id,
+                'content' : content,
+                'user_id': user_id,
+                }
+        )
+
+        return response
+
+
+
+if __name__ == "__main__":
+    # Example usage of the list_forms function
+    dotenv_path = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + "/.env")
+    vcn = HealthieAPIVirtualCareNavigator(dotenv_path=dotenv_path)
+
+    if True:
+        # test endpoint
+        data = {"resource_id": 260040, "resource_id_type": "Note", "event_type": "message.created", "changed_fields": []}
+        response = vcn.endpoint(data=data)
+
+        print(json.dumps(response, indent=4))
 
