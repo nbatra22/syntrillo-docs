efbe45651f3cda0f8de25dfe771ac71abfe2750c
Olivier Delrieu (maxwell) <olivier@15kay.fr>
2024-06-08 16:57:57 +0200

    devices poc

--- apps/PythonAnywhere/website/healthie/iframe_provider_tab/devices.py
+++ apps/PythonAnywhere/website/healthie/iframe_provider_tab/devices.py
@@ -1,21 +1,27 @@
 # Path: ./apps/PythonAnywhere/website/healthie/iframe_provider_tab/devices.py
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, jsonify
 
-import time
+import json
 
 from .post_management import PostManager
 from syntrillo.patient_initialization.accounts_pairing import AccountsPairing
+from syntrillo.pseudonyms_management.lookup_codes_management import LookUpCodesManagement
+from syntrillo.pseudonyms_management.temporary_lookup_codes_management import TemporaryLookUpCodesManagement
 
 iframe_healthie_provider_tab_devices_bp = Blueprint('iframe_healthie_provider_tab_devices_bp', __name__)
+
+# ========================= HTML PAGE ==========================
 
 @iframe_healthie_provider_tab_devices_bp.route('/healthie/iframe_provider_tab/devices', methods=['POST'])
 def iframe_healthie_provider_tab_devices():
     """
     This endpoint is used to display the devices page in the provider tab iframe.
+    It is called by the healthie_iframe_provider_tab index.html
     """
 
     # get all pseudonyms from post temporary identifier
-    post_manager = PostManager(request)
+    post_manager = PostManager()
+    post_manager.get_pseudonyms_from_index_post(request)
 
     # deal with patients not registered at Syntrillo
     if post_manager.patient_not_registered_at_syntrillo:
@@ -26,4 +32,60 @@ def iframe_healthie_provider_tab_devices():
     paired_devices = AccountsPairing.get_paired_devices(syntrillo_internal_key=post_manager.syntrillo_internal_key)
 
 
-    return render_template('healthie/iframe_provider_tab/devices.html')
+    return render_template('healthie/iframe_provider_tab/devices.html',
+                           temporary_lookup_code=post_manager.temporary_lookup_code,
+                           healthie_provider_id=post_manager.healthie_provider_id,
+                           paired_devices=paired_devices,
+                           )
+
+# ========================= ENDPOINTS ==========================
+
+@iframe_healthie_provider_tab_devices_bp.route('/healthie/iframe_provider_tab/devices/tenovi_generate_temporary_pairing_code_form', methods=['POST'])
+def tenovi_generate_temporary_pairing_code_form():
+    """
+    This endpoint generates a pairing code to be entered in the Tenovi platform 'Patient ID' field by the study coordinator.
+
+    """
+
+    # get all pseudonyms from post temporary identifier
+    post_manager = PostManager()
+    post_manager.get_pseudonyms_from_tab_post(request)
+
+    # Call AccountsPairing.create_and_return_unique_temporary_pseudo_code
+    accounts_pairing = AccountsPairing(post_manager.syntrillo_internal_key)
+    temporary_tenovi_pseudo_code = accounts_pairing.create_and_return_unique_temporary_pseudo_code()
+
+    if temporary_tenovi_pseudo_code is None:
+        log = {
+            "success": False,
+            "message": "Error: Temporary Pseudo Code for Tenovi pairing not generated",
+            'temporary_tenovi_pseudo_code': None
+        }
+    else:
+        log = {
+            "success": True,
+            "message": "Temporary Pseudo Code for Tenovi pairing generated successfully",
+            'temporary_tenovi_pseudo_code': temporary_tenovi_pseudo_code
+        }
+
+    return jsonify( log ), 200
+
+
+@iframe_healthie_provider_tab_devices_bp.route('/healthie/iframe_provider_tab/devices/tenovi_pair_devices_form', methods=['POST'])
+def tenovi_pair_devices_form():
+    """
+    This endpoint pairs the devices using the temporary tenovi pseudo code.
+
+    """
+
+    # get all pseudonyms from post temporary identifier
+    post_manager = PostManager()
+    post_manager.get_pseudonyms_from_tab_post(request)
+
+    # pair devices using the temporary pseudo codes
+    pairing = AccountsPairing(post_manager.syntrillo_internal_key)
+    log = pairing.pair_devices_using_temporary_pseudo_code()
+
+    return jsonify( log ), 200
+
+
