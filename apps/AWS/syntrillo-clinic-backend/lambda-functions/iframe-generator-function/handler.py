7b1f64d58145c10ebeb492e9dbc76900b4f65275
Olivier Lemaitre <olemaitre@syntrillo.com>
2024-06-12 17:36:54 +0200

    update iframe-generator handler & add routes/ and syntrillo/ folders symlinks

--- apps/AWS/syntrillo-clinic-backend/lambda-functions/iframe-generator-function/handler.py
+++ apps/AWS/syntrillo-clinic-backend/lambda-functions/iframe-generator-function/handler.py
@@ -1,11 +1,11 @@
 import json
-
 import awsgi
 
 from flask import (
     Flask,
     jsonify,
     render_template,
+    url_for
 )
 
 app = Flask(__name__)
@@ -13,6 +13,15 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("main_page.html")
+
+from routes.healthie.iframe_provider_tab.devices import iframe_healthie_provider_tab_devices_bp
+app.register_blueprint(iframe_healthie_provider_tab_devices_bp)
+
+@app.route('/iframe_healthie_provider_tab')
+def iframe_healthie_provider_tab_index():
+    return render_template("healthie/iframe_provider_tab/index.html",
+                            iframe_healthie_provider_tab_devices_url = '/prod' + url_for("iframe_healthie_provider_tab_devices_bp.iframe_healthie_provider_tab_devices_olemaitre")
+                           )
 
 def handler(event, context):
     print(event)
