cafd8a4334764023219d5147b95c1db39b330ca1
Olivier Lemaitre <olemaitre@syntrillo.com>
2024-06-18 11:30:17 +0200

    add test for pairing code generation an pairing apis

--- apps/AWS/syntrillo-clinic-backend/tests/apis/test-apis.py
+++ apps/AWS/syntrillo-clinic-backend/tests/apis/test-apis.py
@@ -45,6 +45,22 @@ class TestSyntrilloClinicBackendAPIs(unittest.TestCase):
         status_code = requests.post(url, data=device_form_data).status_code
         self.assertEqual(status_code, 200, url)
 
+    def test_iframe_healthie_provider_tab_devices_generate_temporary_pairing_code(self):
+        url = f'https://{iframe_generator_api_id}.execute-api.us-east-1.amazonaws.com/prod/healthie/iframe_provider_tab/devices/tenovi_generate_temporary_pairing_code_form'
+        device_form_data = {
+            'temporary_lookup_code': '69c70ff8-6589-42b2-8b54-1501079441e2',
+        }
+        status_code = requests.post(url, data=device_form_data).status_code
+        self.assertEqual(status_code, 200, url)
+
+    def test_iframe_healthie_provider_tab_devices_pair_devices(self):
+        url = f'https://{iframe_generator_api_id}.execute-api.us-east-1.amazonaws.com/prod/healthie/iframe_provider_tab/devices/tenovi_pair_devices_form'
+        device_form_data = {
+            'temporary_lookup_code': '907c7bef-8295-470f-96b4-bb2e934a7750',
+        }
+        status_code = requests.post(url, data=device_form_data).status_code
+        self.assertEqual(status_code, 200, url)
+
 if __name__ == '__main__':
     unittest.main()
 
---
+++ sources/syntrillo/api_tenovi/test-tenovi-api.py
@@ -0,0 +1,7 @@
+import sys
+sys.path.append('/home/olivier/SyntrilloClinic/sources')
+
+from devices import Devices
+
+devices = Devices()
+print(devices.get_devices_by_pseudo_code('564c8031-da06-4f7e-9057-44c4b790547d'))
