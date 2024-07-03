7da3a818ddf1daa0d338284f47e6397a2b018e89
Olivier Lemaitre <olemaitre@syntrillo.com>
2024-07-03 23:00:19 +0200

    improve comment

--- apps/AWS/syntrillo-clinic-backend/tests/apis/test-apis-foundation.py
+++ apps/AWS/syntrillo-clinic-backend/tests/apis/test-apis-foundation.py
@@ -68,15 +68,16 @@ def display_error_context(url, request):
 
 class TestFoundation(unittest.TestCase):
 
+    # 502 Http Error means the lambda function is not working properly (it returns None for example or it is timedout)
+    # N.B. Keep in mind that lambda time out includes cold start 
+    # (i.e. if the timeout is 3s and the lambda function take 3 seconds to start it will timeout), 
+    # you should then increase the lambda timeout in the cdk stack
+    
     def test_check_connectivity_function_returns_error_if_no_check_selected(self):
         url = f'https://{check_connectivity_api_id}.execute-api.us-east-1.amazonaws.com/sandbox/'
         request = requests.get(url)
         status_code = request.status_code
         self.assertEqual(status_code, 500, display_error_context(url, request))
-        # 502 means the lambda function is not working properly (it returns None for example or it is timedout)
-        # N.B. Keep in mind that lambda time out includes cold start 
-        # (i.e. if the timeout is 3s and the lambda function take 3 seconds to start it will timeout), 
-        # you should then increase the lambda timeout in the cdk stack
 
     def test_check_connectivity_function_internet_ingress(self):
         url = f'https://{check_connectivity_api_id}.execute-api.us-east-1.amazonaws.com/sandbox/'
