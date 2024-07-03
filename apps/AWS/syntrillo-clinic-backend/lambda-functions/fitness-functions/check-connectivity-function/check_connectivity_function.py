20742102de2770a02147890c1959dd5560314ef8
Olivier Lemaitre <olemaitre@syntrillo.com>
2024-07-03 13:39:25 +0200

    add internet egress test

--- apps/AWS/syntrillo-clinic-backend/lambda-functions/fitness-functions/check-connectivity-function/check_connectivity_function.py
+++ apps/AWS/syntrillo-clinic-backend/lambda-functions/fitness-functions/check-connectivity-function/check_connectivity_function.py
@@ -1,18 +1,50 @@
-def handler(event, context):
+import requests
+
+def test_internet_egress():
+    response = requests.get("https://www.example.com")
+    return str(response)
+
+def response_200(message='Hello World'):
     return {
         'statusCode': 200,
-        'body': 'Hello World'
+        'body': message
     }
+
+def response_500(message='Hello World'):
+    return {
+        'statusCode': 500,
+        'body': message
+    }
+
+def handler(event, context):
+    resource_path = event['path']
+
+    if resource_path == "/test_internet_egress":
+        response = test_internet_egress()
+        if response == '<Response [200]>':
+            return response_200('test_internet_egress ok')
+        else:
+            return response_500("Access to internet not available")
+
+    return response_200()
 
 if __name__ == '__main__':
     import unittest
 
     class TestFoundation(unittest.TestCase):
-        def test_handler(self):
+        
+        def test_internet_ingress(self):
             expected = {
                 'statusCode': 200,
                 'body': 'Hello World'
             }
-            self.assertEqual(expected, handler(None, None))
+            self.assertEqual(expected, handler({"path": "/"}, None))
+
+        def test_internet_egress(self):
+            expected = {
+                'statusCode': 200,
+                'body': 'test_internet_egress ok'
+            }
+            self.assertEqual(expected, handler({"path": "/test_internet_egress"}, None))
 
     unittest.main()
--- apps/AWS/syntrillo-clinic-backend/tests/utils/test-local.sh
+++ apps/AWS/syntrillo-clinic-backend/tests/utils/test-local.sh
@@ -1,1 +1,8 @@
-python3 ../../../syntrillo-clinic-backend/lambda-functions/fitness-functions/check-connectivity-function/check_connectivity_function.py
+cd ../../../syntrillo-clinic-backend/lambda-functions/fitness-functions
+python3 ./check-connectivity-function/check_connectivity_function.py
+
+cd -
+cd ../../../syntrillo-clinic-backend
+# sam build --template cdk.out/SyntrilloClinicBackendStack.template.json
+# sam local invoke --template cdk.out/SyntrilloClinicBackendStack.template.json CheckConnectivityFunction |jq .
+# sam local start-api --template cdk.out/SyntrilloClinicBackendStack.template.json
