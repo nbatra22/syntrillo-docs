e5d3651802cce32006bad328cea6b5bc9b1cdea5
Olivier Delrieu (maxwell) <olivier@15kay.fr>
2024-07-05 12:18:56 +0200

    charting note prefill framework

--- apps/PythonAnywhere/website/routes/healthie/iframe_provider_tab/onboarding.py
+++ apps/PythonAnywhere/website/routes/healthie/iframe_provider_tab/onboarding.py
@@ -4,6 +4,7 @@ from flask import Blueprint, render_template, request, jsonify
 from .post_management import PostManager
 
 from syntrillo.patient_onboarding.manager import PatientOnboardingManager
+from syntrillo.charting_note_prefill.handler import ChartingNotePrefillHandler
 
 iframe_healthie_provider_tab_onboarding_bp = Blueprint('iframe_healthie_provider_tab_onboarding_bp', __name__)
 
@@ -25,15 +26,23 @@ def iframe_healthie_provider_tab_onboarding():
         return render_template('healthie/iframe_provider_tab/patient_not_registered.html')
 
     # --------------------------------------------------------------------
-
+    # Onboarding Manager
     onboarding_manager = PatientOnboardingManager()
     patient_status = onboarding_manager.get_user_status(user_id=post_manager.pseudonyms['healthie_user_id'])
     inconsistencies = onboarding_manager.get_inconsistencies(user_id=post_manager.pseudonyms['healthie_user_id'])
+
+    # --------------------------------------------------------------------
+    # Charting Note Prefill
+    charting_note_prefill_handler = ChartingNotePrefillHandler(healthie_user_id=post_manager.pseudonyms['healthie_user_id'])
+    private_folders_and_documents, _ = charting_note_prefill_handler.list_private_folders_and_documents()
+    charting_notes, _ = charting_note_prefill_handler.get_charting_notes()
 
     return render_template('healthie/iframe_provider_tab/onboarding.html',
                            temporary_lookup_code=post_manager.temporary_lookup_code,
                            patient_status=patient_status,
                            inconsistencies=inconsistencies,
+                           private_folders_and_documents=private_folders_and_documents,
+                           charting_notes=charting_notes
                            )
 
 
@@ -77,6 +86,25 @@ def healthie_onboarding_generate_personalized_form():
             "message": "Personalized Intake Form generated successfully",
             'new_form': new_form
         }
+
+    return jsonify( log ), 200
+
+@iframe_healthie_provider_tab_onboarding_bp.route('/healthie/iframe_provider_tab/onboarding/healthie_prefill_charting_note_form', methods=['POST'])
+def healthie_prefill_charting_note_form():
+    """
+
+
+    """
+
+    # get all pseudonyms from post temporary identifier
+    post_manager = PostManager()
+    post_manager.get_pseudonyms_from_tab_post(request)
+
+    # --------------------------------------------------------------------
+
+    charting_note_prefill_handler = ChartingNotePrefillHandler(healthie_user_id=post_manager.pseudonyms['healthie_user_id'])
+
+    log = charting_note_prefill_handler.run_prefill_ai_agent()
 
     return jsonify( log ), 200
 
