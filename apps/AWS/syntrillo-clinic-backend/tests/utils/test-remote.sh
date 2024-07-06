8330e5856ba219cb419e2158624317b02cdc8241
Olivier Lemaitre <olemaitre@syntrillo.com>
2024-07-06 18:44:10 +0200

    refactor test-remote script

--- apps/AWS/syntrillo-clinic-backend/tests/utils/test-remote.sh
+++ apps/AWS/syntrillo-clinic-backend/tests/utils/test-remote.sh
@@ -1,334 +1,115 @@
-# # # TEST IFRAMES
+#!/bin/bash
 
-# # RESOURCE_PATH='/healthie/iframe_provider_tab/system_devices/tenovi_pair_devices_form'
+# Load configuration from environment variables
+FUNCTION_NAME="${FUNCTION_NAME:-IFrameGeneratorFunction}"
+API_NAME="${API_NAME:-IFramGeneratorAPI}"
 
-# # echo
-# # echo "Remote Iframe Test ($RESOURCE_PATH)"
+# Function to invoke a Lambda function
+invoke_lambda_function() {
+    local function_name="$1"
+    local resource_path="$2"
+    local payload="$3"
 
-# # PAYLOAD='{"httpMethod": "POST", "path": "'$RESOURCE_PATH'", "queryStringParameters": "", "body": "temporary_lookup_code=750335a3-d8bb-49cb-9e57-559f6441e099"}'
+    local temp_file
+    out_temp_file="/tmp/$(basename $resource_path).json"
+    log_temp_file="/tmp/$(basename $resource_path).logs"
 
-# # TEST=$(aws lambda invoke \
-# #     --cli-binary-format raw-in-base64-out \
-# #     --function-name IFrameGeneratorFunction \
-# #     --cli-binary-format raw-in-base64-out \
-# #     --payload "$PAYLOAD" \
-# #     --log-type Tail \
-# #     --query 'LogResult' \
-# #     --output text \
-# #     /tmp/test.json > /tmp/test-with-logs.json)
-
-# # ERROR_MESSAGE=$(cat /tmp/test.json | jq .errorMessage)
-
-# # if [ "$ERROR_MESSAGE" == "null" ]; then
-# #     echo "IFrame Generator function ($RESOURCE_PATH) test passed"
-# # else
-# #     echo "IFrame Generator function ($RESOURCE_PATH) test failed"
-# #     cat /tmp/test.json | jq # in the case of "/" $RESOURCE_PATH, file is /tmp/.json
-# #     exit 1
-# # fi
-
-# # STATUS_CODE=$(cat /tmp/test.json | jq .statusCode)
-# # if [ "$STATUS_CODE" == "\"200\"" ]; then
-# #     echo "IFrame Generator function ($RESOURCE_PATH) test passed"
-# # else
-# #     echo "IFrame Generator function test failed"
-# #     cat /tmp/test.json | jq
-# #     cat /tmp/test-with-logs.json | base64 --decode
-# #     exit 1
-# # fi
-
-
-
-# CHECK FOUNDATIONS IFRAMES
-
-RESOURCE_PATHES="\
-/check_internet_ingress \
-/check_internet_egress \
-/check_mysql_database_access \
-"
-
-for resource_path in $RESOURCE_PATHES; do
-    RESOURCE_PATH=$resource_path
-    
-    echo
-    echo "Remote Function Test ($RESOURCE_PATH)"
-
-    TEST=$(aws lambda invoke \
+    aws lambda invoke \
         --cli-binary-format raw-in-base64-out \
-        --function-name CheckConnectivityFunction \
+        --function-name "$function_name" \
         --cli-binary-format raw-in-base64-out \
-        --payload "{ \"path\": \"$RESOURCE_PATH\" }" \
-        /tmp/$RESOURCE_PATH.json)
-
-    ERROR_MESSAGE=$(cat /tmp/$RESOURCE_PATH.json | jq .errorMessage)
-
-    if [ "$ERROR_MESSAGE" == "null" ]; then
-        echo "ConnectivityCheckFunction ($RESOURCE_PATH) test passed"
-    else
-        echo "ConnectivityCheckFunction ($RESOURCE_PATH) test failed"
-        cat /tmp/$RESOURCE_PATH.json | jq
-        exit 1
-    fi
-
-    STATUS_CODE=$(cat /tmp/$RESOURCE_PATH.json | jq .statusCode)
-    if [ "$STATUS_CODE" == "200" ]; then
-        echo "ConnectivityCheckFunction ($RESOURCE_PATH) test passed"
-    else
-        echo "ConnectivityCheckFunction test failed"
-        cat /tmp/$RESOURCE_PATH.json | jq
-        exit 1
-    fi
-
-    echo
-    echo "Remote Api Test ($RESOURCE_PATH)"
-
-    REST_API_ID=$(aws apigateway get-rest-apis --query "items[?name=='CheckConnectivityAPI'].id" --output text)
-    RESOURCE_ID=$(aws apigateway get-resources --rest-api-id $REST_API_ID --query "items[?path=='$RESOURCE_PATH'].id" --output text)
-    TEST=$(aws apigateway test-invoke-method \
-        --rest-api-id $REST_API_ID \
-        --resource-id $RESOURCE_ID \
-        --http-method GET \
-        --path-with-query-string $RESOURCE_PATH \
-        --body '')
-
-    STATUS=$(echo $TEST | jq .status)
-
-    if [ "$STATUS" == "200" ]; then
-        echo "CheckConnectivityAPI ($RESOURCE_PATH) test passed"
-    else
-        echo "CheckConnectivityAPI ($RESOURCE_PATH) test failed"
-        echo $TEST | jq .
-        echo "### LAMBDA FUNCTION RESPONSE (CAN HELP TO UNDERSTAND THE ISSSUE SOMETIMES)"
-        cat /tmp/$RESOURCE_PATH.json | jq .
-        exit 1
-    fi
-done
-
-RESOURCE_PATHES="\
-/check_python_module_import \
-"
-
-for resource_path in $RESOURCE_PATHES; do
-    RESOURCE_PATH=$resource_path
-    
-    echo
-    echo "Remote Function Test ($RESOURCE_PATH)"
-
-    TEST=$(aws lambda invoke \
-        --cli-binary-format raw-in-base64-out \
-        --function-name CheckBehaviourFunction \
-        --cli-binary-format raw-in-base64-out \
-        --payload "{ \"path\": \"$RESOURCE_PATH\" }" \
-        /tmp/$RESOURCE_PATH.json)
-
-    ERROR_MESSAGE=$(cat /tmp/$RESOURCE_PATH.json | jq .errorMessage)
-
-    if [ "$ERROR_MESSAGE" == "null" ]; then
-        echo "CheckBehaviourFunction ($RESOURCE_PATH) test passed"
-    else
-        echo "CheckBehaviourFunction ($RESOURCE_PATH) test failed"
-        cat /tmp/$RESOURCE_PATH.json | jq
-        exit 1
-    fi
-
-    STATUS_CODE=$(cat /tmp/$RESOURCE_PATH.json | jq .statusCode)
-    if [ "$STATUS_CODE" == "200" ]; then
-        echo "CheckBehaviourFunction ($RESOURCE_PATH) test passed"
-    else
-        echo "CheckBehaviourFunction test failed"
-        cat /tmp/$RESOURCE_PATH.json | jq
-        exit 1
-    fi
-
-    echo
-    echo "Remote Api Test ($RESOURCE_PATH)"
-
-    REST_API_ID=$(aws apigateway get-rest-apis --query "items[?name=='CheckBehaviourAPI'].id" --output text)
-    RESOURCE_ID=$(aws apigateway get-resources --rest-api-id $REST_API_ID --query "items[?path=='$RESOURCE_PATH'].id" --output text)
-    TEST=$(aws apigateway test-invoke-method \
-        --rest-api-id $REST_API_ID \
-        --resource-id $RESOURCE_ID \
-        --http-method GET \
-        --path-with-query-string $RESOURCE_PATH \
-        --body '')
-
-    STATUS=$(echo $TEST | jq .status)
-
-    if [ "$STATUS" == "200" ]; then
-        echo "CheckConnectivityAPI ($RESOURCE_PATH) test passed"
-    else
-        echo "CheckConnectivityAPI ($RESOURCE_PATH) test failed"
-        echo $TEST | jq .
-        echo "### LAMBDA FUNCTION RESPONSE (CAN HELP TO UNDERSTAND THE ISSSUE SOMETIMES)"
-        cat /tmp/$RESOURCE_PATH.json | jq .
-        exit 1
-    fi
-done
-
-# TEST IFRAMES (GET)
-
-RESOURCE_PATHES="\
-/ \
-/iframe_healthie_provider_tab \
-"
-
-FUNCTION_NAME="IFrameGeneratorFunction"
-
-for resource_path in $RESOURCE_PATHES; do
-    RESOURCE_PATH=$resource_path
-    PATH_BASE_NAME=$(basename $RESOURCE_PATH)
-
-    echo
-    echo "Remote Iframe Test ($RESOURCE_PATH)"
-
-    PAYLOAD='{"httpMethod": "GET", "path": "'$resource_path'", "queryStringParameters": ""}'
-
-    TEST=$(aws lambda invoke \
-        --cli-binary-format raw-in-base64-out \
-        --function-name $FUNCTION_NAME \
-        --cli-binary-format raw-in-base64-out \
-        --payload "$PAYLOAD" \
-        /tmp/$PATH_BASE_NAME.json)
-
-    ERROR_MESSAGE=$(cat /tmp/$PATH_BASE_NAME.json | jq .errorMessage)
-
-    if [ "$ERROR_MESSAGE" == "null" ]; then
-        echo "IFrame Generator function ($RESOURCE_PATH) test passed"
-    else
-        echo "IFrame Generator function ($RESOURCE_PATH) test failed"
-        cat /tmp/$PATH_BASE_NAME.json | jq # in the case of "/" $RESOURCE_PATH, file is /tmp/.json
-        exit 1
-    fi
-
-    STATUS_CODE=$(cat /tmp/$PATH_BASE_NAME.json | jq .statusCode)
-    if [ "$STATUS_CODE" == "\"200\"" ]; then
-        echo "IFrame Generator function ($RESOURCE_PATH) test passed"
-    else
-        echo "IFrame Generator function test failed"
-        cat /tmp/$PATH_BASE_NAME.json | jq
-        exit 1
-    fi
-
-    echo
-    echo "Remote Api Test ($RESOURCE_PATH)"
-
-    REST_API_ID=$(aws apigateway get-rest-apis --query "items[?name=='IFramGeneratorAPI'].id" --output text)
-    RESOURCE_ID=$(aws apigateway get-resources --rest-api-id $REST_API_ID --query "items[?path=='$RESOURCE_PATH'].id" --output text)
-    TEST=$(aws apigateway test-invoke-method \
-        --rest-api-id $REST_API_ID \
-        --resource-id $RESOURCE_ID \
-        --http-method GET \
-        --path-with-query-string $RESOURCE_PATH \
-        --body '')
-
-    STATUS=$(echo $TEST | jq .status)
-
-    if [ "$STATUS" == "200" ]; then
-        echo "IFrame Generator API ($RESOURCE_PATH) test passed"
-    else
-        echo "IFrame Generator API ($RESOURCE_PATH) test failed"
-        echo $TEST | jq .
-        echo "### LAMBDA FUNCTION RESPONSE (CAN HELP TO UNDERSTAND THE ISSSUE SOMETIMES)"
-        cat /tmp/$PATH_BASE_NAME.json | jq .
-        exit 1
-    fi
-
-done
-
-# TEST IFRAMES (POST)
-
-event='''{
-"temporary_lookup_code": "84bbd0cd-d8d5-40cc-b993-d50f8d98ba66",
-"date_start_ago": "one-week-ago",
-"date_end_ago": "today",
-"blood_pressure_state": "healthy",
-"heart_rate_state": "healthy",
-"steps_state": "healthy",
-"medication_adherence_state": "perfect",
-"medication_expected_pattern": "twice_daily"
-}'''
-
-RESOURCE_PATHES="\
-/healthie/iframe_provider_tab/devices/tenovi_order_new_devices_form \
-/healthie/iframe_provider_tab/system_devices/tenovi_dummy_data_generator_form \
-"
-
-FUNCTION_NAME="IFrameGeneratorFunction"
-
-for resource_path in $RESOURCE_PATHES; do
-    RESOURCE_PATH=$resource_path
-    PATH_BASE_NAME=$(basename $RESOURCE_PATH)
-
-    echo
-    echo "Remote Iframe Test ($RESOURCE_PATH)"
-
-    echo "$PAYLOAD" > /tmp/payload.json
-
-    echo $PAYLOAD
-
-    TEST=$(aws lambda invoke \
-        --cli-binary-format raw-in-base64-out \
-        --function-name $FUNCTION_NAME \
-        --cli-binary-format raw-in-base64-out \
-        --payload "fileb://event-samples/$PATH_BASE_NAME.json" \
+        --payload "$payload" \
         --log-type Tail \
-        --query 'LogResult' \
-        --output text \
-        /tmp/$PATH_BASE_NAME.json > /tmp/$PATH_BASE_NAME.logs)
+        "$out_temp_file" > $log_temp_file
+        
+    local error_message
+    error_message="$(jq '.errorMessage' "$out_temp_file")"
 
-    ERROR_MESSAGE=$(cat /tmp/$PATH_BASE_NAME.json | jq .errorMessage)
+    local status_code
+    status_code="$(jq '.statusCode' "$out_temp_file")"
 
-    if [ "$ERROR_MESSAGE" == "null" ]; then
-        echo "IFrame Generator function ($RESOURCE_PATH) test passed"
+    if [[ "$error_message" == "null" || "$status_code" == "200" ]]; then
+        echo "Function $function_name ($resource_path) test passed"
     else
-        echo "IFrame Generator function ($RESOURCE_PATH) test failed"
-        cat /tmp/$PATH_BASE_NAME.json | jq # in the case of "/" $RESOURCE_PATH, file is /tmp/.json
-        exit 1
+        echo "Function $function_name test failed"
+        echo "status_code: "$status_code
+        echo "error_message: "$error_message
+        jq '.' "$out_temp_file"
+        cat /tmp/$log_temp_file |jq .LogResult | sed 's/^"//' | sed 's/"$//' | base64 --decode
+        return 1
     fi
+}
 
-    STATUS_CODE=$(cat /tmp/$PATH_BASE_NAME.json | jq .statusCode)
-    echo $STATUS_CODE
-    if [ "$STATUS_CODE" == "\"200\"" ]; then
-        echo "IFrame Generator function ($RESOURCE_PATH) test passed"
+# Function to test an API Gateway endpoint
+test_api_gateway_endpoint() {
+    local resource_path="$1"
+    local api_name="$2"
+    local invoke_method="$3"
+
+    local rest_api_id
+    rest_api_id="$(aws apigateway get-rest-apis --query "items[?name=='$api_name'].id" --output text)"
+
+    local resource_id
+    resource_id="$(aws apigateway get-resources --rest-api-id "$rest_api_id" --query "items[?path=='$resource_path'].id" --output text)"
+
+    local test_result
+    test_result="$(aws apigateway test-invoke-method \
+        --rest-api-id "$rest_api_id" \
+        --resource-id "$resource_id" \
+        --http-method $invoke_method \
+        --path-with-query-string "$resource_path" \
+        --body '')"
+
+    local status
+    status="$(echo "$test_result" | jq '.status')"
+
+    if [[ "$status" == "200" ]]; then
+        echo "API $api_name ($resource_path) test passed"
     else
-        echo "IFrame Generator function ($RESOURCE_PATH) test failed"
-        cat /tmp/$PATH_BASE_NAME.json | jq
-        cat /tmp/$PATH_BASE_NAME.logs | base64 --decode
-        exit 1
+        echo "API $api_name ($resource_path) test failed"
+        echo "API OUTPUT"
+        echo "$test_result" | jq '.'
+        echo "LAMBDA OUTPUT"
+        cat /tmp/$log_temp_file |jq .LogResult | sed 's/^"//' | sed 's/"$//' | base64 --decode
+        return 1
     fi
+}
 
+# Define resource paths as arrays
+
+# Test connectivity checks
+CONNECTIVITY_CHECK_PATHS=("/check_internet_ingress" "/check_internet_egress" "/check_mysql_database_access")
+for resource_path in "${CONNECTIVITY_CHECK_PATHS[@]}"; do
+    echo "---"$resource_path
+    invoke_lambda_function "CheckConnectivityFunction" "$resource_path" "{\"path\": \"$resource_path\"}"
+    test_api_gateway_endpoint "$resource_path" "CheckConnectivityAPI" "GET"
     echo
-    echo "Remote Api Test ($RESOURCE_PATH)"
-
-    REST_API_ID=$(aws apigateway get-rest-apis --query "items[?name=='IFramGeneratorAPI'].id" --output text)
-    RESOURCE_ID=$(aws apigateway get-resources --rest-api-id $REST_API_ID --query "items[?path=='$RESOURCE_PATH'].id" --output text)
-    TEST=$(aws apigateway test-invoke-method \
-        --rest-api-id $REST_API_ID \
-        --resource-id $RESOURCE_ID \
-        --http-method POST \
-        --path-with-query-string $RESOURCE_PATH \
-        --body 'temporary_lookup_code=520ca46e-99a5-4bc2-9f32-c0e4c283969c&gateway_id=None&device_bpm_large=yes&device_pillbox=yes&device_watch=yes')
-
-    STATUS=$(echo $TEST | jq .status)
-
-    if [ "$STATUS" == "200" ]; then
-        echo "IFrame Generator API ($RESOURCE_PATH) test passed"
-    else
-        echo "IFrame Generator API ($RESOURCE_PATH) test failed"
-        echo $TEST | jq .
-        echo "### LAMBDA FUNCTION RESPONSE (CAN HELP TO UNDERSTAND THE ISSSUE SOMETIMES)"
-        cat /tmp/$PATH_BASE_NAME.json | jq .
-        exit 1
-    fi
-
 done
 
+# Test behavior checks
+BEHAVIOR_CHECK_PATHS=("/check_python_module_import")
+for resource_path in "${BEHAVIOR_CHECK_PATHS[@]}"; do
+    echo "---"$resource_path
+    invoke_lambda_function "CheckBehaviourFunction" "$resource_path" "{\"path\": \"$resource_path\"}"
+    test_api_gateway_endpoint "$resource_path" "CheckBehaviourAPI" "GET"
+    echo
+done
 
-cd ../..
-python3 tests/apis/test-apis-foundation.py
-cd -
+# Test iframe GET requests
+IFRAME_GET_PATHS=("/" "/iframe_healthie_provider_tab")
+for resource_path in "${IFRAME_GET_PATHS[@]}"; do
+    echo "---"$resource_path
+    invoke_lambda_function "$FUNCTION_NAME" "$resource_path" "{\"httpMethod\": \"GET\", \"path\": \"$resource_path\", \"queryStringParameters\": \"\"}"
+    test_api_gateway_endpoint "$resource_path" "$API_NAME" "GET"
+    echo
+done
 
-cd ../..
-python3 tests/apis/test-apis-iframes.py
-cd -
-
+# Test iframe POST requests
+IFRAME_POST_PATHS=("/healthie/iframe_provider_tab/devices/tenovi_order_new_devices_form" "/healthie/iframe_provider_tab/system_devices/tenovi_dummy_data_generator_form")
+for resource_path in "${IFRAME_POST_PATHS[@]}"; do
+    echo "---"$resource_path
+    payload="$(cat event-samples/$(basename "$resource_path").json)"
+    invoke_lambda_function "$FUNCTION_NAME" "$resource_path" "$payload"
+    test_api_gateway_endpoint "$resource_path" "$API_NAME" "POST"
+    echo
+done
