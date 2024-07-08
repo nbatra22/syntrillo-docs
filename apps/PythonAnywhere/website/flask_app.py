cf35d84539ab5e29a572f825728d2dd44e7a5d6d
Olivier Lemaitre <olemaitre@syntrillo.com>
2024-07-08 17:31:34 +0200

    remove tests_ui from flask app

--- apps/PythonAnywhere/website/flask_app.py
+++ apps/PythonAnywhere/website/flask_app.py
@@ -22,17 +22,6 @@ from api import api_bp
 # Register blueprints
 app.register_blueprint(api_bp)
 
-# => Olivier L Note: testui is not in the requirements.txt
-# ---------- tests ui ---------------------
-# from tests_ui.index import tests_ui_index_bp
-# from tests_ui.tab1 import tests_ui_tab1_bp
-# from tests_ui.tab2 import tests_ui_tab2_bp
-
-# app.register_blueprint(tests_ui_index_bp)
-# app.register_blueprint(tests_ui_tab1_bp)
-# app.register_blueprint(tests_ui_tab2_bp)
-
-
 # ================== Healthie route for webhook endpoints ========================
 
 from routes.healthie.endpoints import healthie_endpoint_bp
