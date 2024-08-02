f7ffc5ac395efa72035f81581db3b08b07aafa10
Olivier Delrieu (maxwell) <olivier@15kay.fr>
2024-08-02 22:46:34 +0200

    UI

--- apps/PythonAnywhere/website/routes/healthie/iframe_provider_tab/care_plan.py
+++ apps/PythonAnywhere/website/routes/healthie/iframe_provider_tab/care_plan.py
@@ -53,7 +53,7 @@ def iframe_healthie_provider_tab_care_plan():
             last_ranges_unit='day',
             use_total=False,
             add_whole_range=True,
-            whole_period_name='Whole Periods',
+            whole_period_name='Whole Time',
         )
 
         summary_df_weekly, _ = drc.get_summary_statistics()
@@ -72,7 +72,7 @@ def iframe_healthie_provider_tab_care_plan():
             last_ranges_unit='week',
             use_total=False,
             add_whole_range=True,
-            whole_period_name='Whole Periods',
+            whole_period_name='Whole Time',
         )
 
         summary_df_monthly, _ = drc.get_summary_statistics()
