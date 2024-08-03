9d91371464c1cc6466dd8ca848f08993cab8718c
Olivier Lemaitre <olemaitre@syntrillo.com>
2024-08-03 14:36:53 +0200

    add body and headers samples for tests

--- apps/AWS/syntrillo-clinic-backend/tests/remote/test-remote-invokes-lambdas-and-apis.sh
+++ apps/AWS/syntrillo-clinic-backend/tests/remote/test-remote-invokes-lambdas-and-apis.sh
@@ -6,8 +6,11 @@ source ~/SyntrilloClinic/apps/AWS/syntrillo-clinic-backend/tests/remote/_invoke-
 IFRAME_GET_PATHS=("/" "/iframe_healthie_provider_tab")
 for resource_path in "${IFRAME_GET_PATHS[@]}"; do
     echo "---"$resource_path
-    invoke_lambda_function "$FUNCTION_NAME" "$resource_path" "{\"httpMethod\": \"GET\", \"path\": \"$resource_path\", \"queryStringParameters\": \"\"}"
-    test_api_gateway_endpoint "$resource_path" "$API_NAME" "GET"
+    payload="{\"httpMethod\": \"GET\", \"path\": \"$resource_path\", \"queryStringParameters\": \"\"}"
+    body="$(cat ~/SyntrilloClinic/apps/AWS/syntrillo-clinic-backend/utils/api-body-and-headers-samples/$(basename "$resource_path").body)"
+    headers="$(cat ~/SyntrilloClinic/apps/AWS/syntrillo-clinic-backend/utils/api-body-and-headers-samples/$(basename "$resource_path").headers)"
+    invoke_lambda_function "$FUNCTION_NAME" "$resource_path" "$payload"
+    test_api_gateway_endpoint "$resource_path" "$API_NAME" "GET" "$headers" "$body"
     echo
 done
 
@@ -15,8 +18,10 @@ done
 IFRAME_POST_PATHS=("/healthie/iframe_provider_tab/status" "/healthie/iframe_provider_tab/devices")
 for resource_path in "${IFRAME_POST_PATHS[@]}"; do
     echo "---"$resource_path
-    payload="$(cat ~/SyntrilloClinic/apps/AWS/syntrillo-clinic-backend/utils/event-samples/$(basename "$resource_path").json)"
+    payload="$(cat ~/SyntrilloClinic/apps/AWS/syntrillo-clinic-backend/utils/lambda-event-samples/$(basename "$resource_path").json)"
+    body="$(cat ~/SyntrilloClinic/apps/AWS/syntrillo-clinic-backend/utils/api-body-and-headers-samples/$(basename "$resource_path").body)"
+    headers="$(cat ~/SyntrilloClinic/apps/AWS/syntrillo-clinic-backend/utils/api-body-and-headers-samples/$(basename "$resource_path").headers)"
     invoke_lambda_function "$FUNCTION_NAME" "$resource_path" "$payload"
-    test_api_gateway_endpoint "$resource_path" "$API_NAME" "POST"
+    test_api_gateway_endpoint "$resource_path" "$API_NAME" "POST" "$headers" "$body" 
     echo
 done
