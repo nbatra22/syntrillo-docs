49136d2dec6a915703c7ac1c87f75a016fb9dfa9
Olivier Lemaitre <olemaitre@syntrillo.com>
2024-08-04 13:40:52 +0200

    test snapshots on simple cdk project

--- apps/AWS/simple-cdk-project/simple_cdk_project/simple_cdk_project_stack.py
+++ apps/AWS/simple-cdk-project/simple_cdk_project/simple_cdk_project_stack.py
@@ -4,16 +4,19 @@ from aws_cdk import (
     # aws_sqs as sqs,
 )
 from constructs import Construct
+from aws_cdk import aws_lambda as _lambda
+from aws_cdk import aws_lambda as _lambda
 
 class SimpleCdkProjectStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "SimpleCdkProjectQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # create a lambda function with python 3.9 runtime
+        lambda_function = _lambda.Function(
+            self,
+            "SimpleLambdaFunction",
+            runtime=_lambda.Runtime.PYTHON_3_10,
+            code=_lambda.Code.from_asset("lambda"),
+            handler="lambda_function.lambda_handler",
+        )
