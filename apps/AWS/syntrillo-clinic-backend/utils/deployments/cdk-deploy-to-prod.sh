0157b29c339a1e73f0223b20a0b88b08b6461be9
Olivier Lemaitre <olemaitre@syntrillo.com>
2024-08-06 09:43:56 +0200

    protect prod deployment script

--- apps/AWS/syntrillo-clinic-backend/utils/deployments/cdk-deploy-to-prod.sh
+++ apps/AWS/syntrillo-clinic-backend/utils/deployments/cdk-deploy-to-prod.sh
@@ -1,2 +1,16 @@
-cd ~/SyntrilloClinic/apps/AWS/syntrillo-clinic-backend
-cdk deploy --profile syntrillo-clinic-prod-deployment --context environment='prod' $@ 
+read -rp "!!! You are deploying to PRODUCTION, are you sure (Yes/no)? " confirmation
+
+if [ "$confirmation" == "Yes" ]; then
+  cd ~/SyntrilloClinic/apps/AWS/syntrillo-clinic-backend
+  echo "Make DIFF with new revision..."
+  cdk diff --profile syntrillo-clinic-prod-deployment --context environment='prod' $@ 
+  read -rp "!!! Are you happy with what will be deployed (Yes/no)? " confirmation
+  if [ "$confirmation" == "Yes" ]; then
+    echo "Start PROD deployment..."
+    cd ~/SyntrilloClinic/apps/AWS/syntrillo-clinic-backend
+    cdk deploy --profile syntrillo-clinic-prod-deployment --context environment='prod' $@ 
+  fi
+else
+  echo "Aborted"
+fi
+
