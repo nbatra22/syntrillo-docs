d77a47fa709e0aaecf6d92849658b77238d17b7a
Olivier Delrieu (maxwell) <olivier@15kay.fr>
2024-08-09 15:02:09 +0200

    added tasks and scripts to run cdk commands

---
+++ system/delrieu/deploy-to-staging.sh
@@ -0,0 +1,7 @@
+#!/bin/bash --login
+
+# Activate the Python environment
+source /home/olivier/projects/Syntrillo/python_environments/p3.9_Syntrillo_Clinic_v7/bin/activate
+
+/home/olivier/projects/Syntrillo/Syntrillo_Clinic/apps/AWS/syntrillo-clinic-backend/utils/deployments/cdk-deploy-to-staging.sh
+
