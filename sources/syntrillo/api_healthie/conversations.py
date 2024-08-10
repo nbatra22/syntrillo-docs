b4c88c3b6f5e54d544ea89c7e5ac3b57be8636b1
Olivier Delrieu (maxwell) <olivier@15kay.fr>
2024-08-10 21:10:10 +0200

    backup

---
+++ sources/syntrillo/api_healthie/conversations.py
@@ -0,0 +1,23 @@
+"""
+
+Within the API, a "Chat" is known as a Conversation. A message in a conversation is a Note object.
+
+"""
+
+
+class HealthieConversation:
+    """
+
+    https://docs.gethealthie.com/docs/#chat
+
+    https://help.gethealthie.com/article/82-overview-chatting-with-a-client
+
+
+
+    """
+
+
+    def __init__(self):
+        pass
+
+
