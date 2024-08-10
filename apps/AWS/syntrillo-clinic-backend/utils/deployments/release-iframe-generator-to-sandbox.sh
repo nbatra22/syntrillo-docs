6535d35bce00ece142f17e2a0c6f0fe8c04e39b5
Olivier Lemaitre <olemaitre@syntrillo.com>
2024-08-10 15:32:21 +0200

    reduce number of log messages

--- apps/AWS/syntrillo-clinic-backend/utils/deployments/release-iframe-generator-to-sandbox.sh
+++
@@ -1,4 +0,0 @@
-~/SyntrilloClinic/apps/AWS/syntrillo-clinic-backend/tests/local/test-local-source-syntrillo.sh && \
-~/SyntrilloClinic/apps/AWS/syntrillo-clinic-backend/tests/local/test-local-flask-app.sh && \
-./deploy-iframe-generator-to-sandbox.sh && \
-~/SyntrilloClinic/apps/AWS/syntrillo-clinic-backend/tests/remote/test-remote-invokes-lambdas-and-apis.sh
