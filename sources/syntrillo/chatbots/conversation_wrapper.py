feb62a81d3f2bb786a666ff61719b11e489c77d9
Olivier Delrieu (maxwell) <olivier@15kay.fr>
2024-08-11 22:47:02 +0200

    UX

--- sources/syntrillo/chatbots/conversation_wrapper.py
+++ sources/syntrillo/chatbots/conversation_wrapper.py
@@ -376,7 +376,7 @@ class ChatBotConversationWrapper:
 if __name__ == '__main__':
 
     # test ChatBotConversationWrapper from a conversation_id
-    conversation_id = '1532280'
+    conversation_id = '1532283'
     wrapper = ChatBotConversationWrapper()
     log = wrapper.load_conversation_from_conversation_id(conversation_id)
 
--- sources/syntrillo/chatbots/dispatcher.py
+++ sources/syntrillo/chatbots/dispatcher.py
@@ -93,7 +93,7 @@ if __name__ == "__main__":
     # dummy run, to test the dispatcher
     # using a real note, which is already in the conversation.
     # Get conversation_id from browser and use conversation wrapper to get the note_id
-    note_id = '270848'
+    note_id = '270867'
 
     data = {"resource_id": note_id, "resource_id_type": "Note", "event_type": "message.created", "changed_fields": []}
     dispatcher = ChatBotsDispatcher()
