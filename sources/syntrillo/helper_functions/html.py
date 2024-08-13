a6ac5b7366f6b33525f777e9caff14048c25e6af
Olivier Delrieu (maxwell) <olivier@15kay.fr>
2024-08-13 12:55:35 +0200

    returns str, deals with md and css

--- sources/syntrillo/helper_functions/html.py
+++ sources/syntrillo/helper_functions/html.py
@@ -1,8 +1,7 @@
 import re
-import bleach
 import markdown
-from markupsafe import Markup
 from bleach.css_sanitizer import CSSSanitizer
+from bleach.sanitizer import Cleaner
 
 
 def remove_html_tags(text: str) -> str:
@@ -19,48 +18,73 @@ def remove_html_tags(text: str) -> str:
     clean = re.sub('<.*?>', '', text)
     return clean
 
-def transform_to_safe_html(response : str) -> str:
+
+def transform_to_safe_html(input_str: str) -> str:
     """
-    Transform a given response to safe HTML.
+    Transform a given input to safe HTML.
 
     This function checks if the response contains HTML or Markdown content
-    and sanitizes it to ensure safe rendering in the browser.
+    and sanitizes it to ensure safe rendering in the browser. If the response
+    does not contain HTML or Markdown, it is returned as-is.
 
     Parameters:
-    - response (str): The input response to transform.
+    - input (str): The input string to transform.
 
     Returns:
-    - Markup: The transformed response as safe HTML.
+    - str: The transformed input as safe HTML.
     """
-    # Define regular expressions for HTML and Markdown patterns
-    html_pattern = re.compile(r'<[^>]+>')
-    markdown_pattern = re.compile(r'^\s*#.*|^[\*\-_]\s')
+    # Define allowed tags and attributes
+    my_allowed_tags = [
+        'h3', 'h4', 'h5', 'p', 'a', 'strong', 'b', 'u', 'i', 'em', 'sub', 'sup',
+        'ul', 'ol', 'li', 'img', 'table', 'tr', 'td', 'th', 'tbody', 'span', 'div', 'br'
+    ]
+    my_allowed_attributes = {
+        'a': ['href', 'title'],
+        'p': ['class'],  # for Bootstrap
+        'img': ['src', 'alt'],
+        'table': ['border', 'cellpadding', 'cellspacing'],
+        'td': ['colspan', 'rowspan'],
+        'span': ['style'],  # for LaTeX
+        'div': ['style'],  # for LaTeX
+    }
 
-    # Define your own allowed tags and attributes
-    my_allowed_tags = ['h3', 'h4', 'h5', 'p', 'a', 'strong', 'b', 'u', 'i', 'em', 'sub', 'sup',
-                       'ul', 'ol', 'li',
-                       'img',
-                       'table', 'tr', 'td', 'th', 'tbody',
-                       'span', 'div',
-                       'br',
-                       ]
-    my_allowed_attributes = {'a': ['href', 'title'],
-                             'p': ['class'],  # for bootstrap
-                             'img': ['src', 'alt'],
-                             'table': ['border', 'cellpadding', 'cellspacing'],
-                             'td': ['colspan', 'rowspan'],
-                             'span' : ['style'], # for LaTex
-                             'div' : ['style'],  # for LaTex
-                             }
-
-    # ---
-    # Use bleach with custom allowed tags and attributes
-    # Initialize a CSSSanitizer
+    # Initialize a CSSSanitizer to allow specific CSS properties
     css_sanitizer = CSSSanitizer(allowed_css_properties=["color", "font-size", "background-color"])
 
-    cleaned_html = bleach.clean(response, tags=my_allowed_tags, attributes=my_allowed_attributes, css_sanitizer=css_sanitizer)
+    # Initialize a bleach Cleaner with the CSS sanitizer
+    cleaner = Cleaner(tags=my_allowed_tags, attributes=my_allowed_attributes, css_sanitizer=css_sanitizer, strip=True)
 
-    html_output = bleach.clean(markdown.markdown(response), tags=my_allowed_tags, attributes=my_allowed_attributes, css_sanitizer=css_sanitizer)
+    # Regex pattern to detect HTML tags
+    html_tag_pattern = re.compile(r'<[^>]+>')
 
-    return str(Markup(html_output))
+    # Regex pattern to detect Markdown (simple heuristic)
+    # More specific Markdown pattern detection (only matches at start of line)
+    markdown_pattern = re.compile(r'^\s*(#{1,6}\s|[*_~`]{1,2}|[>\-+]\s)', re.MULTILINE)
+
+    # Check if the response contains HTML or Markdown
+    if html_tag_pattern.search(input_str):
+        # If HTML is found, clean it
+        cleaned_html = cleaner.clean(input_str)
+        return cleaned_html
+
+    elif markdown_pattern.search(input_str):
+        # If Markdown is found, convert to HTML and clean it
+        html_output = markdown.markdown(input_str)
+        cleaned_html = cleaner.clean(html_output)
+        return cleaned_html
+
+    else:
+        # If no HTML or Markdown is detected, return the response as-is
+        return input_str
+
+if __name__ == '__main__':
+    # Test the helper functions
+    test_html = '<p><b>Hello</b> <a href="https://example.com">World</a></p>'
+    test_markdown = '**Hello** [World](https://example.com)'
+    test_plain_text = 'Hello World'
+
+    print(remove_html_tags(test_html))
+    print(transform_to_safe_html(test_html))
+    print(transform_to_safe_html(test_markdown))
+    print(transform_to_safe_html(test_plain_text))
 
