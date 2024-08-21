32670412dfc9d763f147fb70f434189562cc14e4
Olivier Delrieu (maxwell) <olivier@15kay.fr>
2024-08-21 12:16:43 +0200

    fix

--- sources/syntrillo/system/iframe_validator.py
+++ sources/syntrillo/system/iframe_validator.py
@@ -46,11 +46,11 @@ class IframeValidator:
 
             if referer:
                 referer += '/'
-                referer_is_valid = any(referer.startswith(domain) for domain in self.ALLOWED_DOMAINS), log
+                referer_is_valid = any(referer.startswith(domain) for domain in self.ALLOWED_DOMAINS_PRODUCTION), log
 
             if origin:
                 origin += '/'
-                origin_is_valid = any(origin.startswith(domain) for domain in self.ALLOWED_DOMAINS), log
+                origin_is_valid = any(origin.startswith(domain) for domain in self.ALLOWED_DOMAINS_PRODUCTION), log
 
             return referer_is_valid or origin_is_valid, log
 
