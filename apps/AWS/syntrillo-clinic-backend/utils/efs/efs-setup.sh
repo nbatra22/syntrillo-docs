72f3fb3e979b06b5a7857500e152dba303abffd9
Olivier Lemaitre <olemaitre@syntrillo.com>
2024-08-26 01:59:35 +0200

    freeze requirements.txt v8

--- apps/AWS/syntrillo-clinic-backend/utils/efs/efs-setup.sh
+++ apps/AWS/syntrillo-clinic-backend/utils/efs/efs-setup.sh
@@ -20,4 +20,4 @@ sudo yum install docker
 sudo systemctl start docker
 sudo chmod 666 /var/run/docker.sock
 sudo usermod -a -G docker ec2-user
-docker run --rm -v "$PWD":/app -w /app python:3.10 pip install --no-cache-dir -r requirements.txt --target=/app/shared-python-modules
+docker run --rm -v "$PWD":/app -w /app python:3.10 pip install --no-cache-dir -r requirements.txt --target=.
