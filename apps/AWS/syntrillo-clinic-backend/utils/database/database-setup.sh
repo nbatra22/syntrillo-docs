e6900c0f95962032881749369f3439d83fd3aeab
Olivier Lemaitre <olemaitre@syntrillo.com>
2024-08-26 22:03:20 +0200

    update database setup examples

--- apps/AWS/syntrillo-clinic-backend/utils/database/database-setup.sh
+++ apps/AWS/syntrillo-clinic-backend/utils/database/database-setup.sh
@@ -1,2 +1,10 @@
 # install mariadb/mysql client
 sudo yum install mariadb105
+
+# Create a user that can authenticate with IAM Role
+CREATE USER 'syntrillo_clinic_lambda_user' IDENTIFIED WITH AWSAuthenticationPlugin as 'RDS';
+FLUSH PRIVILEGES; # <== Important or you get an access denied error
+GRANT SELECT, INSERT, UPDATE, DELETE ON syntrillo$HealthInformation.* TO 'syntrillo_clinic_lambda_user'@'%';
+FLUSH PRIVILEGES;
+GRANT SELECT, INSERT, UPDATE, DELETE ON syntrillo$PseudonymManagement.* TO 'syntrillo_clinic_lambda_user'@'%';
+FLUSH PRIVILEGES;
