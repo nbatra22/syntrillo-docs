7d0a2a4751a876bd1b2a7f40faae244b5f4e043d
Olivier Lemaitre <olemaitre@syntrillo.com>
2024-08-31 14:23:01 +0200

    order allowed ips (avoid diff with staging and prod)

--- apps/AWS/syntrillo-clinic-backend/syntrillo_clinic_backend/constructs/iframe_generator_api_endpoint_construct.py
+++ apps/AWS/syntrillo-clinic-backend/syntrillo_clinic_backend/constructs/iframe_generator_api_endpoint_construct.py
@@ -210,15 +210,15 @@ class IFrameGeneratorApiEndpoint(Construct):
         return api
 
     def _resource_policy(self):
-        iframe_allowed_ip_addresses = set(
+        iframe_allowed_ip_addresses = sorted(set(
             ip_info["ip"]
             for ip_info in self.environment_context["iframe_generator_api"]["iframes_allowed_api_adresses"]
-        )
+        ))
 
-        webhook_allowed_ip_addresses = set(
+        webhook_allowed_ip_addresses = sorted(set(
             ip_info["ip"]
             for ip_info in self.environment_context["iframe_generator_api"]["webhooks_allowed_api_adresses"]
-        )
+        ))
 
         allow_all_invokes_policy_statement = iam.PolicyStatement(
             effect=iam.Effect.DENY,
