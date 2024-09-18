1fa065c7b7439adcbd3e8f45e1774792c9429543
Olivier Delrieu (maxwell) <olivier@15kay.fr>
2024-09-18 21:55:42 +0200

    todos

--- sources/syntrillo/chatbots/dispatcher.py
+++ sources/syntrillo/chatbots/dispatcher.py
@@ -51,6 +51,7 @@ class ChatBotsDispatcher:
 
         # ------------------------------
         # dispatch the message to the appropriate chatbot, based on time and several variables
+        # TODO : log error if note['user_id'] empty (when a provider sent the note ?)
         note_creator = self.convo_wrapper.get_note_creator()
         note_content = self.convo_wrapper.get_note_content()
         convo_includes_multiple_clients = self.convo_wrapper.does_convo_includes_multiple_clients()
