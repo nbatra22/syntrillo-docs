b402773416c2c19eee16fc31282c9e670f63730f
Olivier Delrieu (maxwell) <olivier@15kay.fr>
2024-09-28 09:41:28 +0200

    bots option

--- sources/syntrillo/chatbots/dispatcher.py
+++ sources/syntrillo/chatbots/dispatcher.py
@@ -154,7 +154,9 @@ class ChatBotsDispatcher:
                 # if the note creator is an investor (demo tag or specific id), start the bedrock after hours virtual assistant
                 #   production : Demo Patient 'Eleanor Demo': xxxxx ; tagged as 'demo'
                 if note_creator.does_user_have_tag('demo') and note_creator.healthie_user_id == 'xxxxxxx':
-                    v04_after_hours_virtual_assistant_bedrock = True
+                    v01_start_after_hours_support_chatbot = False
+                    v02_start_after_hours_virtual_assistant = False
+                    v04_after_hours_virtual_assistant_bedrock = False
 
 
         # start the chatbot with the conversationWrapper object
