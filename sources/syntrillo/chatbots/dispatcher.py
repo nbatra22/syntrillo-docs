a31a26cddf990d766a8786429718e6730ecbd4f2
Olivier Delrieu (maxwell) <olivier@15kay.fr>
2024-09-28 09:45:53 +0200

    doc

--- sources/syntrillo/chatbots/dispatcher.py
+++ sources/syntrillo/chatbots/dispatcher.py
@@ -151,7 +151,7 @@ class ChatBotsDispatcher:
             else:
                 # place holder for patient interaction with chatbot
 
-                # if the note creator is an investor (demo tag or specific id), start the bedrock after hours virtual assistant
+                # if the note creator is an investor (demo tag or specific id), start the after hours bot
                 #   production : Demo Patient 'Eleanor Demo': xxxxx ; tagged as 'demo'
                 if note_creator.does_user_have_tag('demo') and note_creator.healthie_user_id == 'xxxxxxx':
                     v01_start_after_hours_support_chatbot = False
