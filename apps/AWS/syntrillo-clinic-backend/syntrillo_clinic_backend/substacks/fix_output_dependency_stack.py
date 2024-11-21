09b48c6c6c0ac64db95c841cee1579bd88c6498f
Olivier Lemaitre <olemaitre@syntrillo.com>
2024-11-21 08:20:12 +0100

    fix output dependency

--- apps/AWS/syntrillo-clinic-backend/syntrillo_clinic_backend/substacks/fix_output_dependency_stack.py
+++ apps/AWS/syntrillo-clinic-backend/syntrillo_clinic_backend/substacks/fix_output_dependency_stack.py
@@ -28,10 +28,10 @@ class FixOutputDependencyStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, stack: Construct, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        self.database = stack
+        self.llm_server = stack
 
         ssm.StringParameter(
             self, "TemporaryFixOutputDependencyParameter",
             parameter_name="/tmp/fix_output_dependency",
-            string_value=self.database.admin_secret.secret_arn,
+            string_value=self.llm_server.security_group.security_group_id,
         )
