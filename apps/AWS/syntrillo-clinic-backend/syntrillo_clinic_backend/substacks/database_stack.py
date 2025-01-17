b5ba7dde2fce67430538397bf93e990273e775bf
Olivier Lemaitre <o.lemaitre@welcloud.io>
2025-01-17 21:56:40 +0100

    add peering with analytics and export values

--- apps/AWS/syntrillo-clinic-backend/syntrillo_clinic_backend/substacks/database_stack.py
+++ apps/AWS/syntrillo-clinic-backend/syntrillo_clinic_backend/substacks/database_stack.py
@@ -36,6 +36,8 @@ class DatabaseStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         self.environment_context = environment_context
+        self.aws_environment = environment_context["environment_name"]
+
         self.network = network
 
         self.termination_protection = self.environment_context["stacks-termination-protection"]
@@ -124,6 +126,20 @@ class DatabaseStack(Stack):
             ec2.Port.tcp(3306),
             description=f"Allow inbound traffic from Linux Bastion Host on port 3306"
         )
+
+        if self.aws_environment == "staging":
+            DMS_instance_security_group_id = Fn.import_value("SyntrilloAnalyticsNetworkDMSSecurityGroupId")
+            DMS_instance_imported_security_group_id = ec2.SecurityGroup.from_security_group_id(
+                self,
+                "DMSInstanceImportedSecurityGroup",
+                security_group_id=DMS_instance_security_group_id
+            )
+
+            self.db_from_snapshot_security_group.add_ingress_rule(
+                DMS_instance_imported_security_group_id,
+                ec2.Port.tcp(3306),
+                description=f"Allow inbound traffic from Analytics DMS Instance on port 3306"
+            )
 
         message_endpoint_function_security_group_id = Fn.import_value("MessageEndpointFunctionSecurityGroup")
         message_endpoint_function_imported_security_group = ec2.SecurityGroup.from_security_group_id(
