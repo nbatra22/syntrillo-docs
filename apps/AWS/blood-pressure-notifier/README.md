c2b4a93b3288e49c3b42ab79ec08c66c247018c4
Olivier Lemaitre <olemaitre@syntrillo.com>
2025-01-20 20:02:28 +0100

    clean unused folder

--- apps/AWS/blood-pressure-notifier/README.md
+++
@@ -1,4 +0,0 @@
-# Blood pressure notification service
-
-This project serves as a template for developing and deploying Python lambda functions
-to AWS.
--- apps/AWS/blood-pressure-notifier/lambda/blood-pressure-notifier/tenovi_measurements.py
+++
@@ -1,8 +0,0 @@
-def get_tenovi_measurements(
-    patient_id: str,
-    device_id: str,
-    from_date: str,
-    to_date: str,
-) -> dict:
-    print('Implement API calls to Tenovi here')
-    return {}
