2a7c016770b7675c56c2169f1502c5d0e9eefbf2
Olivier Lemaitre <olemaitre@syntrillo.com>
2025-01-30 06:01:41 +0100

    upate deployment utils scripts

--- apps/AWS/syntrillo-analytics-backend/utils/deployments/diff-deployment-staging-prod-policy.sh
+++ apps/AWS/syntrillo-analytics-backend/utils/deployments/diff-deployment-staging-prod-policy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 profile="syntrillo-clinic-staging"
-latest_version=$(aws iam --profile $profile list-policies --query 'Policies[?PolicyName==`SyntrilloClinicBackendCdkCloudFormationExecutionPolicyProdlike`].DefaultVersionId' --output text)
-aws iam --profile $profile get-policy-version --policy-arn arn:aws:iam::021891579520:policy/SyntrilloClinicBackendCdkCloudFormationExecutionPolicyProdlike  --version-id "$latest_version" \
+latest_version=$(aws iam --profile $profile list-policies --query 'Policies[?PolicyName==`SyntrilloAnalyticsBackendCdkCloudFormationExecutionPolicyProdlike`].DefaultVersionId' --output text)
+aws iam --profile $profile get-policy-version --policy-arn arn:aws:iam::021891579520:policy/SyntrilloAnalyticsBackendCdkCloudFormationExecutionPolicyProdlike  --version-id "$latest_version" \
 |jq 'del(.PolicyVersion.VersionId, .PolicyVersion.CreateDate)' > staging-policy.json
 sed -i 's/021891579520/381491864638/g' staging-policy.json
 
 profile="syntrillo-clinic-prod-deployment"
-latest_version=$(aws iam --profile $profile list-policies --query 'Policies[?PolicyName==`SyntrilloClinicBackendCdkCloudFormationExecutionPolicy`].DefaultVersionId' --output text)
-aws iam --profile $profile get-policy-version --policy-arn arn:aws:iam::381491864638:policy/SyntrilloClinicBackendCdkCloudFormationExecutionPolicy  --version-id "$latest_version" \
+latest_version=$(aws iam --profile $profile list-policies --query 'Policies[?PolicyName==`SyntrilloAnalyticsBackendCdkCloudFormationExecutionPolicy`].DefaultVersionId' --output text)
+aws iam --profile $profile get-policy-version --policy-arn arn:aws:iam::381491864638:policy/SyntrilloAnalyticsBackendCdkCloudFormationExecutionPolicy  --version-id "$latest_version" \
 |jq 'del(.PolicyVersion.VersionId, .PolicyVersion.CreateDate)' > prod-policy.json
 
 
