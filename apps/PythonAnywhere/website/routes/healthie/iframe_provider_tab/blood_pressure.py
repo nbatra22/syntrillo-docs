66a7a0ad6533ec967b1319f8b65df1f48f747ea6
Josh Pascual <joshapascual@gmail.com>
2025-02-19 13:54:16 -0800

    download button working

--- apps/PythonAnywhere/website/routes/healthie/iframe_provider_tab/blood_pressure.py
+++ apps/PythonAnywhere/website/routes/healthie/iframe_provider_tab/blood_pressure.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, jsonify, current_app, abort, Response, send_file
-
+import pandas as pd
 import json
 
 from .post_management import PostManager
@@ -79,16 +79,43 @@ def iframe_healthie_provider_tab_blood_pressure():
     analysis_table_with_inception = data_reporting_blood_pressure.calculate_since_baseline(metadata, analysis_table)
     extremes = data_reporting_blood_pressure.calculate_extremes()
 
-    if is_pdf == True:
-        # Convert rendered HTML to PDF
-        bp_analysis_pdf = data_reporting_blood_pressure.generate_pdf(analysis_table_with_inception, extremes)
+    # if is_pdf == True:
+    #     # Convert rendered HTML to PDF
+    #     bp_analysis_pdf = data_reporting_blood_pressure.generate_pdf(analysis_table_with_inception, extremes)
 
-        # Return the PDF for download
-        return send_file(bp_analysis_pdf, as_attachment=True, download_name="blood-pressure-report.pdf")
+    #     # Return the PDF for download
+    #     return send_file(bp_analysis_pdf, as_attachment=True, download_name="blood-pressure-report.pdf")
 
     analysis_html = analysis_table_with_inception.to_html(classes="table table-striped")
+    analysis_json = analysis_table_with_inception.to_json()
+    extremes_json = extremes.to_json()
 
     return render_template(
         'healthie/iframe_provider_tab/blood_pressure.html',
         analysis_html=analysis_html,
+        analysis_json=analysis_json,
+        extremes_json=extremes_json
     )
+
+@iframe_healthie_provider_tab_bp_analysis.route('/healthie/iframe_provider_tab/blood_pressure/download', methods=['GET','POST'])
+def iframe_healthie_provider_tab_download_bp_pdf():
+    analysis_json = request.args.get('analysis_json')
+    extremes_json = request.args.get('extremes_json')
+
+    if not analysis_json or not extremes_json:
+        return "Error, Insufficient data received", 400
+
+    post_manager = PostManager()
+    post_manager.get_pseudonyms_from_tab_post(request)
+
+    # deal with patients not registered at Syntrillo
+    if post_manager.patient_not_registered_at_syntrillo:
+        return render_template('healthie/iframe_provider_tab/patient_not_registered.html')
+
+    data_reporting_blood_pressure = BloodPressureAnalysis(post_manager.syntrillo_internal_key)
+
+    analysis_df = pd.read_json(analysis_json)
+    extremes_df = pd.read_json(extremes_json)
+    bp_pdf = data_reporting_blood_pressure.generate_pdf(analysis=analysis_df, extremes=extremes_df)
+
+    return send_file(bp_pdf, as_attachment=True, download_name="BP-report.pdf", mimetype="application/pdf")
