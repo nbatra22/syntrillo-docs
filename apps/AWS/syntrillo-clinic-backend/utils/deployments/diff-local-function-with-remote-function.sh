8cac99918ea4ce858b8205976e410de489c4ab1c
Olivier Lemaitre <olemaitre@syntrillo.com>
2025-02-27 14:28:54 +0100

    rename deployment scripts

--- apps/AWS/syntrillo-clinic-backend/utils/deployments/diff-local-function-with-remote-function.sh
+++ apps/AWS/syntrillo-clinic-backend/utils/deployments/diff-local-function-with-remote-function.sh
@@ -1,17 +1,17 @@
 if [ "$1" == "" -o "$2" == "" ]; then
-    echo "usage: $0 function_name  function_folder"
+    echo "usage: $0 function_folder function_name"
     exit
 fi
 
 arg_1=$(basename "$1")
 arg_2=$(basename "$2")
 
-PROFILE="--profile syntrillo-clinic-staging"
+PROFILE="--profile syntrillo-clinic-staging-deployment"
 if [ -n "$CODEBUILD_BUILD_ID" ]; then
     PROFILE=""
 fi
 
-REMOTE_CODE_URL=$(aws $PROFILE lambda get-function --function-name $arg_1 --query 'Code.Location' --output text)
+REMOTE_CODE_URL=$(aws $PROFILE lambda get-function --function-name $arg_2 --query 'Code.Location' --output text)
 
 if [ -d "/tmp/remote_code" ]; then
     rm -r /tmp/remote_code
@@ -24,9 +24,9 @@ fi
 curl -L -o /tmp/remote_code.zip "$REMOTE_CODE_URL"
 unzip -q -o /tmp/remote_code.zip -d /tmp/remote_code
 
-function_name="../../lambda-functions/$arg_2"
-function_folder="/tmp/remote_code"
+function_folder="../../lambda-functions/$arg_1"
+remote_code="/tmp/remote_code"
 
-echo $function_name $function_folder
+echo $function_folder $remote_code
 
-diff -r $function_name $function_folder
+diff -r $function_folder $remote_code
