0f5e7894761d0ddc47d49220fd81b6aea56f2c06
Olivier Lemaitre <olemaitre@syntrillo.com>
2025-02-27 15:56:48 +0100

    update symlink script

--- apps/AWS/syntrillo-clinic-backend/utils/deployments/symlinks-recreate.sh
+++ apps/AWS/syntrillo-clinic-backend/utils/deployments/symlinks-recreate.sh
@@ -32,6 +32,7 @@ create_symlinks "../../lambda-functions/iframe-generator-function/" "routes stat
 create_symlinks "../../lambda-functions/message-endpoint-function/" "routes static syntrillo templates api.py"
 create_symlinks "../../lambda-functions/blood-pressure-notification-function/" "syntrillo"
 create_symlinks "../../lambda-functions/remote-monitoring-data-sync-function/" "syntrillo"
+create_symlinks "../../lambda-functions/healthie-data-ingestor-function/" "syntrillo"
 create_symlinks "../../lambda-functions/pii-data-sync-function/" "syntrillo"
 
 create_symlinks "../../../../PythonAnywhere/website/static/healthie/documents/" "questionnaire_template_latest.xlsx"
