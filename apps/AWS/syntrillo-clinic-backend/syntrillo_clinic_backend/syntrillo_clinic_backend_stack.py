8922535ef853038c68d1d59758fd29e1048d3978
Olivier Lemaitre <o.lemaitre@welcloud.io>
2025-03-18 17:23:50 +0100

    add more snapshot tests

--- apps/AWS/syntrillo-clinic-backend/syntrillo_clinic_backend/syntrillo_clinic_backend_stack.py
+++ apps/AWS/syntrillo-clinic-backend/syntrillo_clinic_backend/syntrillo_clinic_backend_stack.py
@@ -115,14 +115,14 @@ class SyntrilloClinicBackendStack(Stack):
             storage=self.storage,
         )
 
-        backupStack = SyntrilloClinicBackupStack(
+        self.backup = SyntrilloClinicBackupStack(
             self, "BackupStack",
             environment_context=self.environment_context,
             database=self.database,
         )
 
         if self.aws_environment == 'staging':
-            backupStack = DeploymentPipelinesStack(
+            self.pipelines = DeploymentPipelinesStack(
                 self, "DeploymentPipelinesStack",
                 environment_context=self.environment_context,
             )
