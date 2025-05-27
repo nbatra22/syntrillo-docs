e6297400ee67e2e8adbbd3eaed6c0b7674714078
Olivier Lemaitre <o.lemaitre@welcloud.io>
2025-05-27 08:49:02 +0200

    use node 20 in deployment pipeline

--- apps/AWS/syntrillo-clinic-backend/syntrillo_clinic_backend/substacks/deployment_pipelines_stack.py
+++ apps/AWS/syntrillo-clinic-backend/syntrillo_clinic_backend/substacks/deployment_pipelines_stack.py
@@ -71,6 +71,7 @@ class DeploymentPipelinesStack(Stack):
                     "install": {
                         "commands": [
                             "cd apps/AWS/syntrillo-clinic-backend",
+                            "nvm use 20",
                             "npm install -g aws-cdk",
                             "python -m pip install -r requirements.txt",
                             "pip install pytest",
@@ -202,6 +203,7 @@ class DeploymentPipelinesStack(Stack):
                     "install": {
                         "commands": [
                             "cd apps/AWS/syntrillo-clinic-backend",
+                            "nvm use 20",
                             "npm install -g aws-cdk",
                             "python -m pip install -r requirements.txt"
                         ]
