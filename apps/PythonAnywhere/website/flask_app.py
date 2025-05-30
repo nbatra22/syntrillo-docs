e9797b41cf7154f224107397b27ae602aac4e598
Josh Pascual <joshapascual@gmail.com>
2025-05-30 15:56:42 -0700

    routes + html connected

--- apps/PythonAnywhere/website/flask_app.py
+++ apps/PythonAnywhere/website/flask_app.py
@@ -62,6 +62,7 @@ app.register_blueprint(iframe_healthie_provider_tab_system_devices_bp)
 
 # ----------- provider sidebar ---------------------
 from routes.healthie.iframe_provider_sidebar.index import iframe_healthie_provider_sidebar_index_bp
+from routes.healthie.iframe_provider_sidebar.billing import iframe_healthie_provider_sidebar_billing_bp
 from routes.healthie.iframe_provider_sidebar.status import iframe_healthie_provider_sidebar_status_bp
 from routes.healthie.iframe_provider_sidebar.questionnaire import iframe_healthie_provider_sidebar_questionnaire_bp
 from routes.healthie.iframe_provider_sidebar.questionnaire_database import iframe_healthie_provider_sidebar_questionnaire_database_bp
@@ -69,6 +70,7 @@ from routes.healthie.iframe_provider_sidebar.dumps import iframe_healthie_provid
 from routes.healthie.iframe_provider_sidebar.system import iframe_healthie_provider_sidebar_system_bp
 
 app.register_blueprint(iframe_healthie_provider_sidebar_index_bp)
+app.register_blueprint(iframe_healthie_provider_sidebar_billing_bp)
 app.register_blueprint(iframe_healthie_provider_sidebar_status_bp)
 app.register_blueprint(iframe_healthie_provider_sidebar_questionnaire_bp)
 app.register_blueprint(iframe_healthie_provider_sidebar_questionnaire_database_bp)
--- apps/PythonAnywhere/website/routes/healthie/iframe_provider_sidebar/billing.py
+++ apps/PythonAnywhere/website/routes/healthie/iframe_provider_sidebar/billing.py
@@ -9,8 +9,8 @@ iframe_healthie_provider_sidebar_billing_bp = Blueprint('iframe_healthie_provide
 
 # ========================= HTML PAGE ==========================
 
-@iframe_healthie_provider_sidebar_billing_bp.route('/healthie/iframe_provider_billing/status', methods=['POST'])
-def iframe_healthie_provider_billing():
+@iframe_healthie_provider_sidebar_billing_bp.route('/healthie/iframe_provider_sidebar/billing', methods=['POST'])
+def iframe_healthie_provider_sidebar_billing():
     """
     This endpoint is used to display the status page in the provider billing iframe.
     It is called by the healthie_iframe_provider_billing index.html
@@ -36,16 +36,16 @@ def iframe_healthie_provider_billing():
     # --------------------------------------------------------------------
 
     # Create an instance of HealthieAPI with the provided API key and organization
-    utils_api = HealthieUtils()
+    # utils_api = HealthieUtils()
 
     # Example: Get organization details
-    organization_details = utils_api.get_organization_details()
+    # organization_details = utils_api.get_organization_details()
 
     return render_template(
         'healthie/iframe_provider_sidebar/billing.html',
-        healthie_provider_id=healthie_provider_id,
-        organization_details=organization_details,
-        )
+        # healthie_provider_id=healthie_provider_id,
+        # organization_details=organization_details,
+    )
 
 # ========================= ENDPOINTS ==========================
 
