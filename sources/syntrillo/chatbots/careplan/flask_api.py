a06679bbc9f45f4e188dd51cae51d634d1f61449
Olivier Lemaitre <o.lemaitre@welcloud.io>
2025-06-03 08:54:36 +0200

    set debug=False for flask api

--- sources/syntrillo/chatbots/careplan/flask_api.py
+++ sources/syntrillo/chatbots/careplan/flask_api.py
@@ -21,4 +21,4 @@ def handle_query():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=False)
