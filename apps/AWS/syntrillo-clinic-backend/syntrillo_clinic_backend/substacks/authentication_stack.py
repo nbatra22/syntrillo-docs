6dc6cec838cc2a369ff2434d3c1dfe7d6a0b9ba7
Olivier Lemaitre <o.lemaitre@welcloud.io>
2025-06-29 21:28:11 +0200

    update authentication stack

--- apps/AWS/syntrillo-clinic-backend/syntrillo_clinic_backend/substacks/authentication_stack.py
+++ apps/AWS/syntrillo-clinic-backend/syntrillo_clinic_backend/substacks/authentication_stack.py
@@ -19,52 +19,31 @@ class AuthenticationStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, environment_context, **kwargs) -> None:
         super().__init__(scope, construct_id)
 
-        # Cognito User Pool (moved up to be available for Lambda environment variables)
+        # Create Cognito User Pool
         user_pool = cognito.UserPool(
             self, "SyntrilloClinicUserPool",
             user_pool_name="SyntrilloClinicUserPool",
+            sign_in_aliases=cognito.SignInAliases(email=True),
             auto_verify=cognito.AutoVerifiedAttrs(email=True),
-            standard_attributes=cognito.StandardAttributes(
-                email=cognito.StandardAttribute(required=True, mutable=False)
-            ),
-            # Configure password policy for new users
             password_policy=cognito.PasswordPolicy(
                 min_length=8,
                 require_lowercase=True,
                 require_uppercase=True,
                 require_digits=True,
-                require_symbols=False
-            ),
-            # Configure account recovery settings
-            account_recovery=cognito.AccountRecovery.EMAIL_ONLY
+                require_symbols=True
+            )
         )
 
-        # User Pool Client
+        # Create User Pool Client
         user_pool_client = cognito.UserPoolClient(
             self, "SyntrilloClinicUserPoolClient",
-            user_pool=user_pool,
             user_pool_client_name="SyntrilloClinicUserPoolClient",
-            generate_secret=False,
+            user_pool=user_pool,
             auth_flows=cognito.AuthFlow(
                 user_password=True,
-                user_srp=True,
-                admin_user_password=True
+                user_srp=True
             ),
-            o_auth=cognito.OAuthSettings(
-                flows=cognito.OAuthFlows(
-                    authorization_code_grant=True,
-                    implicit_code_grant=True
-                ),
-                scopes=[
-                    cognito.OAuthScope.OPENID,
-                    cognito.OAuthScope.EMAIL,
-                    cognito.OAuthScope.PROFILE
-                ],
-                callback_urls=["https://4q497eesgi.execute-api.eu-west-1.amazonaws.com/prod/login.html"]
-            ),
-            supported_identity_providers=[
-                cognito.UserPoolClientIdentityProvider.COGNITO
-            ]
+            generate_secret=False
         )
 
         CfnOutput(self, "SyntrilloClinicAuthenticationUserPoolId", value=user_pool.user_pool_id, export_name="SyntrilloClinic-Authentication-UserPool-Id")
