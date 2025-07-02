0799e4e35d8c67cd2b6a93793fbea1d7ec3d008d
Olivier Lemaitre <o.lemaitre@welcloud.io>
2025-07-02 13:25:59 +0200

    move candid billing ingestor to tasks

--- apps/AWS/syntrillo-clinic-backend/syntrillo_clinic_backend/constructs/tasks/candid_billing_ingestor_function_construct.py
+++ apps/AWS/syntrillo-clinic-backend/syntrillo_clinic_backend/constructs/tasks/candid_billing_ingestor_function_construct.py
@@ -98,7 +98,7 @@ class CandidBillingIngestor(Construct):
             vpc = self.vpc,
             handler="handler.handler",
             runtime=_lambda.Runtime.PYTHON_3_10,
-            code=_lambda.Code.from_asset("lambda-functions/candid-billing-ingestor-function", exclude=['.env', '__pycache__']),
+            code=_lambda.Code.from_asset("lambda-functions/tasks/candid-billing-ingestor-function", exclude=['.env', '__pycache__']),
             params_and_secrets=params_and_secrets,           
             filesystem =_lambda.FileSystem.from_efs_access_point(
                 self.clinic_storage_efs_access_point_shared_python_modules,
