aab6300e218e2528f1ae9da3146beebac8026a64
Olivier Lemaitre <o.lemaitre@welcloud.io>
2025-08-04 16:29:45 +0200

    replace function name in lambda construct

--- apps/AWS/syntrillo-clinic-backend/utils/development/create-new-lambda-construct.sh
+++ apps/AWS/syntrillo-clinic-backend/utils/development/create-new-lambda-construct.sh
@@ -16,16 +16,19 @@ model_name=$(echo $2 | cut -d'=' -f2 | cut -d'/' -f2) # e.g. blood_pressure_anal
 
 model_base_name=$(echo $model_name | cut -d'.' -f1) # e.g. blood_pressure_analysis_function_construct
 model_construct_name=$(echo $model_base_name | sed 's/_function_construct//') # e.g. blood_pressure_analysis
+model_lambda_function_folder_prefix=$(echo $model_construct_name | tr '_' '-') # e.g. blood-pressure-analysis
 model_construct_name_camel_case=$(echo $model_construct_name | sed 's/^\([a-z]\)/\U\1/' | sed 's/_\([a-z]\)/\U\1/g') # e.g. BloodPressureAnalysis
 
 new_construct_file=$1 # e.g. heart_rate_analysis_workflow_construct.py
 new_construct_base_name=$(echo $new_construct_file | cut -d'.' -f1) # e.g. heart_rate_analysis_function_construct
 new_construct_name=$(echo $new_construct_base_name | sed 's/_function_construct//')  # e.g. heart_rate_analysis
+new_construct_lambda_function_folder_prefix=$(echo $new_construct_name | tr '_' '-') # e.g. heart-rate-analysis
 new_construct_name_camel_case=$(echo $new_construct_name | sed 's/^\([a-z]\)/\U\1/' | sed 's/_\([a-z]\)/\U\1/g') # e.g. HeartRateAnalysis
 
 cd syntrillo_clinic_backend/constructs
 cp $model_category/$model_name $model_category/$new_construct_file
 sed -i "s/$model_construct_name_camel_case/$new_construct_name_camel_case/g" $model_category/$new_construct_file
+sed -i "s/$model_lambda_function_folder_prefix/$new_construct_lambda_function_folder_prefix/g" $model_category/$new_construct_file
 cd - > /dev/null
 
 echo "-----"
