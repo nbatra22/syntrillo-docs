6f0dc7e926acd1bd7a8d46d95ecb8060f1063173
alexscannon <acannon37@gatech.edu>
2025-08-20 17:45:03 -0500

    Reverting old form repsponse saving constraint

--- apps/AWS/syntrillo-clinic-backend/lambda-functions/tasks/healthie-data-ingestor-function/services/form_responses.py
+++ apps/AWS/syntrillo-clinic-backend/lambda-functions/tasks/healthie-data-ingestor-function/services/form_responses.py
@@ -436,6 +436,9 @@ class FormResponseService:
                         created_at
                     )
                     VALUES (%s, %s, %s,REPLACE(%s, '\n', '|'), %s)
+                    ON DUPLICATE KEY UPDATE
+                        answer=VALUES(answer),
+                        created_at=VALUES(created_at);
                 """
 
                 # Convert to list of tuples for executemany
