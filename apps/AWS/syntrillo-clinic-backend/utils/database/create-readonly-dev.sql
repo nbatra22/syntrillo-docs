5951d676f2da4be39c587bfbeaaafa9404960320
Olivier Lemaitre <o.lemaitre@welcloud.io>
2026-01-09 07:56:48 +0100

    update database dev user permissions with update & insert

--- apps/AWS/syntrillo-clinic-backend/utils/database/create-readonly-dev.sql
+++ apps/AWS/syntrillo-clinic-backend/utils/database/create-readonly-dev.sql
@@ -2,7 +2,7 @@ DROP USER 'syntrillo_clinic_readonly_dev';
 CREATE USER 'syntrillo_clinic_readonly_dev' IDENTIFIED BY 'xxx';
 FLUSH PRIVILEGES; # <== Important or you get an access denied error
 
-GRANT SELECT ON syntrillo$HealthInformation.* TO 'syntrillo_clinic_readonly_dev'@'%';
+GRANT SELECT, INSERT, UPDATE ON syntrillo$HealthInformation.* TO 'syntrillo_clinic_readonly_dev'@'%';
 FLUSH PRIVILEGES;
 GRANT SELECT ON syntrillo$PseudonymManagement.* TO 'syntrillo_clinic_readonly_dev'@'%';
 FLUSH PRIVILEGES;
